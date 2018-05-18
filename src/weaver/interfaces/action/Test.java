8be2a090cd4b9fa5374d06600ead8282f5e916f1
jiangyanlong <304940334@qq.com>
2018-05-18 16:03:53 +0800

    write a test java file

---
+++ src/weaver/interfaces/action/Test.java
@@ -0,0 +1,10 @@
+package weaver.interfaces.action;
+
+/**
+ * Created by jiangyanlong on 2018/5/18.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is a test java file");
+    }
+}
