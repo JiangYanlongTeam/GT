f09ecdd035a3e1e89386197026ac15212571660d
jiangyanlong <304940334@qq.com>
2018-05-18 19:02:01 +0800

    提交配置信息

--- src/weaver/interfaces/action/Test.java
+++ src/weaver/interfaces/action/Test.java
@@ -1,10 +1,25 @@
 package weaver.interfaces.action;
 
+import weaver.docs.webservices.DocService;
+import weaver.docs.webservices.DocServiceImpl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by jiangyanlong on 2018/5/18.
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is a test java file");
+
+        DocService service = new DocServiceImpl();
+        try {
+            service.getDocCount("11");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
